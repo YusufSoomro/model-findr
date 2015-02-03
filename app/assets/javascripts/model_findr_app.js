8bf03c2898c8598a4a9ed9e1074931e77ad33afd
YusufSoomro <yusufsoomro1@gmail.com>
2015-02-03 15:11:08 -0800

    Changed from coffeescript backbone to javascript backbone

---
+++ app/assets/javascripts/model_findr_app.js
@@ -0,0 +1,13 @@
+window.ModelFindrApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  ModelFindrApp.initialize();
+});
--- app/assets/javascripts/model_findr_app.js.coffee
+++
@@ -1,9 +0,0 @@
-window.ModelFindrApp =
-  Models: {}
-  Collections: {}
-  Views: {}
-  Routers: {}
-  initialize: -> alert 'Hello from Backbone!'
-
-$(document).ready ->
-  ModelFindrApp.initialize()
